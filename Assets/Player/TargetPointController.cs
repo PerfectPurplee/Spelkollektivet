b61b2f3c7c180946fdd8af43a63d0b5658928471
Qubek <qbojarowski@gmail.com>
2025-06-17 10:41:54 +0200

    target point fix

--- Assets/Player/TargetPointController.cs
+++ Assets/Player/TargetPointController.cs
@@ -16,11 +16,11 @@ public class TargetPointController : MonoBehaviour
     private void Update()
     {
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit[] hits = Physics.RaycastAll(mouseRay, 1000, physicsLayer);
-        if (hits.Length != 0)
+        if (Physics.Raycast(mouseRay, out RaycastHit hit, 10000, physicsLayer))
         {
-            transform.position = hits[0].point;
+            transform.position = hit.point;
         }
+        
         //transform.position = mouseRay.GetPoint(
         //    -Vector3.Dot(mouseRay.origin, Vector3.up) / 
         //    Vector3.Dot(mouseRay.direction, Vector3.up));
