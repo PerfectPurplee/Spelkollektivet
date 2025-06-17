ba68191afbba045bddce48b5e7efc8db226c63da
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-06-17 17:26:51 +0200

    xd

---
+++ Assets/Audio/AudioClipsRefsSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AudioClipsRefsSO : ScriptableObject {
+
+    public AudioClip[] shieldHit;
+    public AudioClip[] skeletonHurt;
+    public AudioClip xp;
+}
