e831085bb688b84f55e21fe5c5945eadade39d7b
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-06-17 21:08:12 +0200

    started sound effect setup

--- Assets/UI/MainMenuUI.cs
+++ Assets/UI/MainMenuUI.cs
@@ -1,20 +1,31 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour {
-    
+    public static MainMenuUI Instance { get; private set; }
     [SerializeField] private Button playButton;
     [SerializeField] private Button settingsButton;
     [SerializeField] private Button quitButton;
 
+    public event EventHandler OnButtonPressed;
+
     private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("More than one instance of MainMenuUI");
+        }
+        Instance = this;
+        
         playButton.onClick.AddListener(() => {
+            OnButtonPressed?.Invoke(this, EventArgs.Empty);
             Loader.LoadScene(Loader.Scene.UIScene);
         });
         settingsButton.onClick.AddListener(() => {
+            OnButtonPressed?.Invoke(this, EventArgs.Empty);
             Debug.Log("settings button clicked");
         });
         quitButton.onClick.AddListener(() => {
+            OnButtonPressed?.Invoke(this, EventArgs.Empty);
             Application.Quit();
         });
     }
