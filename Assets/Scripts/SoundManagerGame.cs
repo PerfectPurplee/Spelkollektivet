00d3ca34f5e160b7b397abd78867edcb6736db71
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-06-18 10:26:38 +0200

    sound manager split into scene versions

--- Assets/Scripts/SoundManagerGame.cs
+++ Assets/Scripts/SoundManagerGame.cs
@@ -2,20 +2,19 @@ using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class SoundManager : MonoBehaviour {
-    
-    public static SoundManager Instance { get; private set; }
+public class SoundManagerGame : MonoBehaviour {
+    public static SoundManagerGame Instance { get; private set; }
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
-    
+
     // private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
     private float volume;
-    
-    
-    
+
+
     private void Awake() {
         if (Instance != null) {
             Debug.LogError("More than one instance of SoundManager");
         }
+
         Instance = this;
 
         // volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
@@ -23,21 +22,16 @@ public class SoundManager : MonoBehaviour {
     }
 
     private void Start() {
-        MainMenuUI.Instance.OnButtonPressed += MainMenuUI_OnButtonPressed;
         PauseMenuUI.Instance.OnButtonPressed += PauseMenuUI_OnButtonPressed;
         XPManager.Instance.OnLevelUp += XPManager_OnLevelUp;
     }
 
     private void XPManager_OnLevelUp(object sender, EventArgs e) {
-        PlaySound(audioClipRefsSO.levelUp, Camera.main.transform.position, 50f);
+        PlaySound(audioClipRefsSO.levelUp, Camera.main.transform.position);
     }
 
     private void PauseMenuUI_OnButtonPressed(object sender, EventArgs e) {
         PlaySound(audioClipRefsSO.menuOption, Vector3.zero);
-    }
-
-    private void MainMenuUI_OnButtonPressed(object sender, EventArgs e) {
-        PlaySound(audioClipRefsSO.menuCategory, Vector3.zero);
     }
 
 
@@ -48,7 +42,4 @@ public class SoundManager : MonoBehaviour {
     private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplayer = 1f) {
         AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplayer * volume);
     }
-    
-
-
 }
---
+++ Assets/Scripts/SoundManagerMenu.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SoundManagerMenu : MonoBehaviour {
+    public static SoundManagerMenu Instance { get; private set; }
+    [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+
+    // private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
+    private float volume;
+
+
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("More than one instance of SoundManager");
+        }
+
+        Instance = this;
+        
+        // volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
+        volume = 1f;
+    }
+
+    private void Start() {
+        MainMenuUI.Instance.OnButtonPressed += MainMenuUI_OnButtonPressed;
+    }
+
+
+    private void MainMenuUI_OnButtonPressed(object sender, EventArgs e) {
+        PlaySound(audioClipRefsSO.menuCategory, Vector3.zero);
+    }
+
+
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f) {
+        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
+    }
+
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplayer = 1f) {
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplayer * volume);
+    }
+}
