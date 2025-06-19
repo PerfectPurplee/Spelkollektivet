5d029c4afda7f1b62eb9194d7033e51ccfab4ffe
Szymon Balcerzak <simonbalcerzak@gmail.com>
2025-06-19 21:22:51 +0200

    some bullet hell update

--- Assets/Boss/BossBulletHellAttack.cs
+++ Assets/Boss/BossBulletHellAttack.cs
@@ -1,65 +1,58 @@
 using System.Collections.Generic;
-using Boss;
 using Enemies;
 using Interface;
 using UnityEngine;
 
-public class BossBulletHellAttack : BossAttack {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
-    }
+namespace Boss {
+    public class BossBulletHellAttack : BossAttack {
+        public override float TriggerDistance => 20f;
+        public override AttackType AttackType => AttackType.Ranged;
+        public override float AttackDuration => 5f;
+        public override int Damage => 5;
 
-    // Update is called once per frame
-    void Update() {
-    }
-
-    public override float TriggerDistance => 20f;
-    public override AttackType AttackType => AttackType.Ranged;
-    public override float AttackDuration => 5f;
-    public override int Damage => 10;
-
-    public override string AnimatorAttackName {
-        get => "BulletAttack";
-        set { }
-    }
-
-    [SerializeField] protected List<BossDamageApplier> attackDamageApplierList;
-    [SerializeField] private int projectileCount = 36; // Number of projectiles around the boss
-    [SerializeField] private float radius = 2f; // Distance from the boss
-
-    public override bool TryAttack() {
-        if (this.IsOnCooldown) return false;
-        if (GameBoss.distanceToPlayer < this.TriggerDistance && GameBoss.BossState != BossState.Attacking) {
-            InvokeOnBossAttack();
-            CreateDamageAppliers();
-            LastAttackTime = Time.time;
-            return true;
+        public override string AnimatorAttackName {
+            get => "BulletAttack";
+            set { }
         }
 
-        return false;
-    }
+        [SerializeField] protected List<BossDamageApplier> attackDamageApplierList;
+        [SerializeField] private int projectileCount = 36; // Number of projectiles around the boss
+        [SerializeField] private float radius = 2f; // Distance from the boss
+
+        public override bool TryAttack() {
+            if (this.IsOnCooldown) return false;
+            if (GameBoss.distanceToPlayer < this.TriggerDistance && GameBoss.BossState != BossState.Attacking) {
+                InvokeOnBossAttack();
+                CreateDamageAppliers();
+                LastAttackTime = Time.time;
+                return true;
+            }
+
+            return false;
+        }
 
 
-    private void CreateDamageAppliers() {
-        if (attackDamageApplierList == null || attackDamageApplierList.Count == 0)
-            return;
+        private void CreateDamageAppliers() {
+            if (attackDamageApplierList == null || attackDamageApplierList.Count == 0)
+                return;
 
-        GameObject bulletPrefab = attackDamageApplierList[0].gameObject;
+            GameObject bulletPrefab = attackDamageApplierList[0].gameObject;
 
-        for (int i = 0; i < projectileCount; i++) {
-            float angle = i * (360f / projectileCount);
-            float rad = angle * Mathf.Deg2Rad;
+            for (int i = 0; i < projectileCount; i++) {
+                float angle = i * (360f / projectileCount);
+                float rad = angle * Mathf.Deg2Rad;
 
-            Vector3 offset = new Vector3(Mathf.Cos(rad), 0f, Mathf.Sin(rad)) * radius;
-            Vector3 spawnPos = transform.position + offset;
+                Vector3 offset = new Vector3(Mathf.Cos(rad), 0f, Mathf.Sin(rad)) * radius;
+                Vector3 spawnPos = transform.position + offset;
 
-            // Look direction: away from the boss (like an explosion)
-            Quaternion rotation = Quaternion.LookRotation(offset.normalized);
+                // Look direction: away from the boss (like an explosion)
+                Quaternion rotation = Quaternion.LookRotation(offset.normalized);
 
-            GameObject instance = Instantiate(bulletPrefab, spawnPos, rotation);
+                GameObject instance = Instantiate(bulletPrefab, spawnPos, rotation);
 
-            if (instance.TryGetComponent<IDamageApplier>(out var applier)) {
-                applier.Attack = this;
+                if (instance.TryGetComponent<IDamageApplier>(out var applier)) {
+                    applier.Attack = this;
+                }
             }
         }
     }
