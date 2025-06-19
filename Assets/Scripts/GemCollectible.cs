9ec6074131a91e7d5261919d42596e49760f3be0
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-06-19 15:00:23 +0200

    added gem collectibles

---
+++ Assets/Scripts/GemCollectible.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class GemCollectible : MonoBehaviour {
+    
+    public static event EventHandler OnGemCollected;
+    private void OnTriggerEnter(Collider other) {
+        if (other.TryGetComponent<Player.Player>(out Player.Player player)) {
+            
+            OnGemCollected?.Invoke(this, EventArgs.Empty);
+            Destroy(gameObject);
+        }
+    }
+}
